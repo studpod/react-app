5f44c6ac97f6b794b77a0e2576a197ce939097bc
studpod <podobin455467@gmail.com>
2024-09-16 17:42:22 +0300

    twoo commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import ReactDOM from 'react-dom/client';
+import React, {useState} from 'react';
+import AddItemForm from './AddItemForm';
+
+function ItemList(props) {
+  return <li>Item { props.index }</li>;
+}
+
+function Item() {
+  const [items, setItems] = useState(['1', '2', '3']);
+  const addItem = (newItem) => {
+    setItems([...items, newItem]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+	    <h1>My Items list</h1>
+	    <ul>
+       {items.map((item, idx) =>
+         <ItemList key={idx} index={item} />)}
+      </ul>
+      <AddItemForm addItem={addItem}/>
+    </>
   );
 }
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Item />);
+
+
+export default Item;
