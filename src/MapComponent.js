819bd5bb1bb559cec9cf723727ce440e37ed47a9
studpod <podobin455467@gmail.com>
2024-12-05 12:28:06 +0200

    Using the React-Leaflet library for interactive maps

---
+++ src/MapComponent.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
+
+const MapComponent = () => {
+  const position = [46.6354, 32.6169]; // Координати центру Херсона
+
+  return (
+    <MapContainer center={position} zoom={13} style={{ height: "100vh", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          Моє рідне місто: <strong>Херсон</strong>.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
